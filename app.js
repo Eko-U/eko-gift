1075749e12d35f8228a4ae34328cbac0d1324039
ekogift <ekogift2000@gmail.com>
2025-06-17 23:55:18 +0100

    include original porfolio pix

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const el = document.querySelector(".list");
+const mainList = document.querySelector(".list");
+const sideList = document.querySelector(".side-list");
 const sideNav = document.querySelector(".side-nav");
 const overlayEl = document.querySelector(".overlay");
 
 const sideMenuBtn = document.querySelector(".menu-outline");
 const closeBtn = document.querySelector(".close-outline");
 
-el.addEventListener("click", function (e) {
+const smoothScrollFn = function (e) {
   e.preventDefault();
+
+  sideNav.classList.add("hidden");
+  overlayEl.classList.add("hidden");
+  document.body.classList.remove("no-scroll");
+
   let elID = e.target.href;
 
   if (elID === undefined) return;
   elID = elID.split("#")[1];
-  console.log(elID);
 
   const ele = document.getElementById(elID);
-  console.log(ele);
   ele.scrollIntoView({
     behavior: "smooth",
   });
-});
+};
 
 function toggleFn(e) {
   e.preventDefault();
@@ -26,6 +30,9 @@ function toggleFn(e) {
   overlayEl.classList.toggle("hidden");
   document.body.classList.toggle("no-scroll");
 }
+
+mainList.addEventListener("click", smoothScrollFn);
+sideList.addEventListener("click", smoothScrollFn);
 
 sideMenuBtn.addEventListener("click", toggleFn);
 closeBtn.addEventListener("click", toggleFn);
